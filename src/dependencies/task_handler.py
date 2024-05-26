f7c42720569627c1c68da77541e4c70acb7c29bd
Alan J Bibins <alanjbibins@gmail.com>
2024-05-26 11:09:37 +0530

    Added task_handler

---
+++ src/dependencies/task_handler.py
@@ -0,0 +1,16 @@
+from task import Task 
+
+class TaskHandler:
+    """
+    Class that handles creation of tasks along with operations for them
+    """
+    def make_new_task():
+        ...
+
+    def delete_task():
+        ...
+
+    def create_task_list():
+        ...
+
+
