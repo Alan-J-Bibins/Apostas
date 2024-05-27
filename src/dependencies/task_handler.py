36ccc048f9cf4989c7f184bb8296715f60c8bd2c
Alan J Bibins <alanjbibins@gmail.com>
2024-05-27 22:08:13 +0530

    added details about functions in task_handler

--- src/dependencies/task_handler.py
+++ src/dependencies/task_handler.py
@@ -1,16 +1,22 @@
-from task import Task 
+from task import Task
+
 
 class TaskHandler:
     """
     Class that handles creation of tasks along with operations for them
     """
-    def make_new_task():
-        ...
 
-    def delete_task():
-        ...
+    def make_new_task(self):
+        """
+        Makes new task and adds it to database
+        """
 
-    def create_task_list():
-        ...
+    def delete_task(self):
+        """
+        Deletes task from database
+        """
 
-
+    def create_task_list(self):
+        """
+        Creates list topics to be studied for the day
+        """
