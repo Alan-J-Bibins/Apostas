4b7b2b56bac1e9b8836a1ce5b4c25f8ec965d1ef
Alan J Bibins <alanjbibins@gmail.com>
2024-06-01 12:01:05 +0530

    added function to access functionalities of the application

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 
 def main():
-    ...
+    cog()
+
+def cog():
+    choice = int(input("[1]Add New Topic\nEnter:"))
+    match(choice):
+        case 1:
+            ...
 
 if __name__ == "__main__":
     main()
